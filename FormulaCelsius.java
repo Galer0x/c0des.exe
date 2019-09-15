d5ad6e8a777301b3f59da2930984ac440b47d5f6
Celestino <54960648+Galer0x@users.noreply.github.com>
2019-09-15 16:20:11 -0400

    Calculo de Celsius/Fahrenheit

---
+++ FormulaCelsius.java
@@ -0,0 +1,30 @@
+
+package Celsius;
+import java.util.Scanner;
+
+public class FormulaCelsius {
+    
+    public static void main(String[] args){
+        
+   double formula;
+   double g = 0;
+    
+    formula=(9/5*g+32);
+    Scanner leitor = new Scanner(System.in);
+    System.out.println(" ENTRE COM O VALOR DE GRAUS");
+    g=leitor.nextDouble();
+    
+    System.out.println(" O VALOR CONVERTIDO EH: "+formula);
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+    }
+    
+}

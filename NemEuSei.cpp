4ff9d3c400e341c939884a5041eecbcee299a019
Celestino <54960648+Galer0x@users.noreply.github.com>
2019-09-18 13:21:53 -0400

    CODIGO SIMPLES DE FOR

---
+++ NemEuSei.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	int a,pares;
+	cout << "DIGITE UM NUMERO: " << endl;
+	cin >> a;
+	for (int i=0;i<a;i++){
+	cout << i << endl;		
+	}
+	cout << "\n";
+		for (int i=0;i<=a;i++){
+			if (i	% 2 ==	0){
+		cout << "NUMERO: " << i << " EH UM PAR DESSE INTERVALO! " << endl; 
+}	
+}
+	
+	
+}

6728fe3788ffc62f934df811c2a9f4707f521ad4
Celestino <54960648+Galer0x@users.noreply.github.com>
2019-09-18 09:57:21 -0400

    Add files via upload

---
+++ Soma3Pares.java
@@ -0,0 +1,40 @@
+
+package SomaPares;
+import java.util.Scanner;
+
+
+public class Soma3Pares {
+    
+    public static void main(String[] args){
+    
+        Scanner leitor = new Scanner(System.in);
+        int n1,n2,n3;
+        float soma;
+        
+      
+        
+        System.out.println("DIGITE O VALOR DE N1: ");
+        n1=leitor.nextInt();
+        System.out.println("DIGITE O VALOR DE N2: ");
+        n2=leitor.nextInt();
+    
+        
+        n3=(n1+n2);
+        
+        System.out.println("A SOMA DOS PARES GERA O 3° NUMERO!");
+        System.out.println("ENTAO, O 3° NUMERO EH: "+n3);
+        
+        
+        
+        
+        
+   
+        
+        
+        
+        
+        
+        
+    }
+    
+}
